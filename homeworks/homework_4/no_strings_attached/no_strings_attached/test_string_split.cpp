1f0b0b782dcac3baa096b58b63ff68dcb3b8f050
ilarioazzollini <ilario.azzollini92@gmail.com>
2023-04-25 11:48:14 +0200

    add tests for overloaded Split

--- homeworks/homework_4/no_strings_attached/no_strings_attached/test_string_split.cpp
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/test_string_split.cpp
@@ -36,4 +36,35 @@ TEST(StringSplit, ApplicationTest) {
     EXPECT_EQ(expected_result, result);
 }
 
+TEST(StringSplitWithExtraInput, DelimiterNotFound) {
+    std::string input = "This is a string";
+    std::string delimiter = "z";
+    std::vector<std::string> result = no_strings_attached::string_split::Split(input, delimiter, 10);
+    EXPECT_EQ(input, result[0]);
+}
+
+TEST(StringSplitWithExtraInput, DelimiterFound) {
+    std::string input = "This is a string";
+    std::string delimiter = "is";
+    std::vector<std::string> expected_result{"Th", " ", " a string"};
+    std::vector<std::string> result = no_strings_attached::string_split::Split(input, delimiter, 3);
+    EXPECT_EQ(expected_result, result);
+}
+
+TEST(StringSplitWithExtraInput, SpaceDelimiter) {
+    std::string input = "This is a string";
+    std::string delimiter = " ";
+    std::vector<std::string> expected_result{"This", "is"};
+    std::vector<std::string> result = no_strings_attached::string_split::Split(input, delimiter, 2);
+    EXPECT_EQ(expected_result, result);
+}
+
+TEST(StringSplitWithExtraInput, ApplicationTest) {
+    std::string input = "hello world";
+    std::string delimiter = " ";
+    std::vector<std::string> expected_result{"hello"};
+    std::vector<std::string> result = no_strings_attached::string_split::Split(input, delimiter, 1);
+    EXPECT_EQ(expected_result, result);
+}
+
 // EXPECT_EQ will always continue the execution of the tests while ASSERT_EQ will not (if it fails)
