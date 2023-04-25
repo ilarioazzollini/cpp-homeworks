8c4f06bb1b014281d84b3105aa1ea58d6df907e4
ilarioazzollini <ilario.azzollini92@gmail.com>
2023-04-25 18:24:18 +0200

    add trim tests

--- homeworks/homework_4/no_strings_attached/examples/trim_strings.cpp
+++ homeworks/homework_4/no_strings_attached/examples/trim_strings.cpp
@@ -4,15 +4,6 @@
 #include <no_strings_attached/string_trim.h>
 
 int main() {
-  std::string result1 = no_strings_attached::string_trim::Trim(" hello ", ' ', Side::kLeft);
-  std::cout << "'" << result1 << "'" << std::endl;
-
-  std::string result2 = no_strings_attached::string_trim::Trim(" hello ", ' ', Side::kRight);
-  std::cout << "'" << result2 << "'" << std::endl;
-
-  std::string result3 = no_strings_attached::string_trim::Trim(" hello ", ' ', Side::kBoth);
-  std::cout << "'" << result3 << "'" << std::endl;
-
   std::string input{};
   std::string result{};
 
--- homeworks/homework_4/no_strings_attached/no_strings_attached/test_string_trim.cpp
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/test_string_trim.cpp
@@ -3,6 +3,27 @@
 #include <no_strings_attached/string_trim.h>
 #include <gtest/gtest.h>
 
-TEST(StringTrim, TrivialTest) {
-    EXPECT_EQ(0, 0);
+TEST(StringTrim, LeftTest) {
+    std::string input{" hello world "};
+    char char_to_trim = ' ';
+    Side side = Side::kLeft;
+    std::string expected_result{"hello world "};
+    std::string result = no_strings_attached::string_trim::Trim(input, char_to_trim, side);
+    EXPECT_EQ(expected_result, result);
+}
+
+TEST(StringTrim, RightTest) {
+    std::string input{" hello world "};
+    char char_to_trim = ' ';
+    Side side = Side::kRight;
+    std::string expected_result{" hello world"};
+    std::string result = no_strings_attached::string_trim::Trim(input, char_to_trim, side);
+    EXPECT_EQ(expected_result, result);
+}
+
+TEST(StringTrim, ApplicationTest) {
+    std::string input{" hello world "};
+    std::string expected_result{"hello world"};
+    std::string result = no_strings_attached::string_trim::Trim(input);
+    EXPECT_EQ(expected_result, result);
 }
